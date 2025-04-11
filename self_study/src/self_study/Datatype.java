cdd31dde55dfe092f82db32a4b075c17d6c0c216
sH. <28824568+sH1222J@users.noreply.github.com>
2025-04-12 02:00:45 +0900

    최초 시작

---
+++ self_study/src/self_study/Datatype.java
@@ -0,0 +1,14 @@
+package self_study;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+// System.out.println 단축키
+		// syso 컨트롤 스페이스 엔터
+		System.out.println();
+		System.out.println();
+		
+
+	}
+
+}
